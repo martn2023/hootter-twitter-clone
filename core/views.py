53ce5b571883602ca6d9b21cae5d905548a99077
Martin Lo <mdangerlo@gmail.com>
2024-04-10 20:28:22 +0700

    core app, home pathing to unauthenticated

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# View for authenticated users
+@login_required(login_url='/login/')  # Redirects to /login/ if not authenticated
+def home_authenticated(request):
+    return render(request, 'core/home_authenticated.html')
+
+# View for unauthenticated users
+def home_unauthenticated(request):
+    return render(request, 'core/home_unauthenticated.html')
