e325cee75e7b6a8194986908bfd1f4f46a0b9d5c
Martin Lo <mdangerlo@gmail.com>
2024-04-12 20:17:05 +0700

    settings.py accesses static files, owl logo shows up on home_unauthenticated

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 # urls.py
-
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+# Add static files serving for development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
