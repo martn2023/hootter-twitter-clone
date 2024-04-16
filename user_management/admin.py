d79d14c04275120476a4a8cec41c996b02cc82c7
Martin Lo <mdangerlo@gmail.com>
2024-04-16 22:48:33 +0900

    account registration works, but getting error msg about user ID already existing

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
+
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import ExtendedUserProfile
 
-class ExtendedUserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'bio',)  # Customize the fields displayed in the admin panel
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
+    # You can keep the rest of the settings from BaseUserAdmin or customize as needed.
 
-# Register the ExtendedUserProfileAdmin class with the admin site
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+class ExtendedUserProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_user_id', 'user', 'bio')
+
+    def get_user_id(self, obj):
+        return obj.user.id
+
+    get_user_id.short_description = 'User ID'
+
+
 admin.site.register(ExtendedUserProfile, ExtendedUserProfileAdmin)
