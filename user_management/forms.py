7707e5b5c136653c833327469c706268d0e64548
Martin <42924628+martn2022@users.noreply.github.com>
2024-05-07 21:30:42 -0400

    bio information now updates immediately after acct creation

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import transaction
+from .models import ExtendedUserProfile  # Import the ExtendedUserProfile model
 
 class ExtendedUserCreationForm(UserCreationForm):
     bio = forms.CharField(max_length=300, required=False)
@@ -10,5 +12,12 @@ class ExtendedUserCreationForm(UserCreationForm):
         fields = ('username', 'password1', 'password2', 'email', 'bio')
 
     def save(self, commit=True):
-        user = super().save(commit=commit)  # Just save the User object
-        return user
+        with transaction.atomic():
+            user = super().save(commit=False)
+            if commit:
+                user.save()
+                # Assuming the profile is automatically created by the signal on user save
+                profile, created = ExtendedUserProfile.objects.get_or_create(user=user)
+                profile.bio = self.cleaned_data['bio']
+                profile.save()
+            return user
